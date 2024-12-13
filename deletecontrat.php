7125a8422bcacd184120104cb7eab0fe3ff5a95d
ELmmostaineMohammed <mohammedelmostaine@gmail.com>
2024-12-13 19:32:33 +0100

    modification au niveau du style

--- deletecontrat.php
+++ deletecontrat.php
@@ -7,11 +7,9 @@ if ($connection->connect_error) {
     die("Erreur de connexion : " . $connection->connect_error);
 }
 
-// Vérifier si l'id est passé en paramètre
 if (isset($_GET["id"])) {
     $id = $_GET["id"];
 
-    // Préparer la requête de suppression
     $stmt = mysqli_query($connection,"DELETE FROM contrats WHERE Numero = '$id'");
     header("Location: Contratslist.php");
 
